47055844424fba013eee519aebb1903e52c34a18
Khaushal <khaushal12@gmail.com>
2024-01-30 18:00:35 -0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Team-AJ-KD-K-D-K-A"
+include(":app")
